ad81ac600e0e56c60ba65706d3180b32bbcfc90b
ratulhasan <ratuljh@gmail.com>
2022-07-13 22:12:17 +0600

    Handling requests (GET)

--- 20make_requests/main.go
+++ 20make_requests/main.go
@@ -9,11 +9,9 @@ import (
 )
 
 const getUrl = "http://localhost:2000/get"
-const postUrl = "http://localhost:2000/post"
 
 func main() {
-	//PerformGetRequest(getUrl)
-	PerformPostJsonRequest(postUrl)
+	PerformGetRequest(getUrl)
 }
 
 func PerformGetRequest(url string) {
@@ -44,32 +42,4 @@ func PerformGetRequest(url string) {
 	var responseString strings.Builder
 	responseString.Write(body)
 	fmt.Println("Response:", responseString.String())
-}
-
-// PerformPostJsonRequest Make post request.
-func PerformPostJsonRequest(url string) {
-	// Fake json payload data
-	formJsonData := strings.NewReader(`{"name":"John Doe","age":30, "city":"New York", "country":"USA", "email":"john@doe.com", "phone":"123456789"}`)
-
-	// Make request
-	response, err := http.Post(url, "application/json", formJsonData)
-	if err != nil {
-		return
-	}
-
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(response.Body)
-
-	fmt.Println("Status:", response.Status)
-	fmt.Println("Headers:", response.Header)
-	bytesContent, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return
-	}
-
-	fmt.Println("Body:", string(bytesContent))
 }
