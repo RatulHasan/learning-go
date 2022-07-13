90ef9669c47e576f1c53c529003254127c9a1880
ratulhasan <ratuljh@gmail.com>
2022-07-13 18:05:29 +0600

    Handling requests (POST)

--- 20make_requests/main.go
+++ 20make_requests/main.go
@@ -8,10 +8,12 @@ import (
 	"strings"
 )
 
-const MYURL = "https://dokan-app.test/wp-json/wp/v2/posts"
+const getUrl = "http://localhost:2000/get"
+const postUrl = "http://localhost:2000/post"
 
 func main() {
-	PerformGetRequest(MYURL)
+	//PerformGetRequest(getUrl)
+	PerformPostJsonRequest(postUrl)
 }
 
 func PerformGetRequest(url string) {
@@ -42,4 +44,32 @@ func PerformGetRequest(url string) {
 	var responseString strings.Builder
 	responseString.Write(body)
 	fmt.Println("Response:", responseString.String())
+}
+
+// PerformPostJsonRequest Make post request.
+func PerformPostJsonRequest(url string) {
+	// Fake json payload data
+	formJsonData := strings.NewReader(`{"name":"John Doe","age":30, "city":"New York", "country":"USA", "email":"john@doe.com", "phone":"123456789"}`)
+
+	// Make request
+	response, err := http.Post(url, "application/json", formJsonData)
+	if err != nil {
+		return
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(response.Body)
+
+	fmt.Println("Status:", response.Status)
+	fmt.Println("Headers:", response.Header)
+	bytesContent, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return
+	}
+
+	fmt.Println("Body:", string(bytesContent))
 }
